52cf55d8ac7b8a2bfdac7e2796e4c03c8bb2aaae
SirStehn <kaste20@student.sdu.dk>
2021-10-29 16:34:12 +0200

    fix

--- ProjektX2.0/Assets/Scenes/Scripts/HealthBarScript.cs
+++ ProjektX2.0/Assets/Scenes/Scripts/HealthBarScript.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class HealthBarScript : MonoBehaviour
 {
-    Image healthBar;
+    
+    public Image healthBar;
     float maxHealth = 100f;
     public static float health;
-    
+
     // Start is called before the first frame update
-    void Start() {
+    public void Start() {
         healthBar = GetComponent<Image> ();
-        public static float health;
+        health = maxHealth;
     }
 
     // Update is called once per frame
-    void Update() {
+    public void Update() {
         healthBar.fillAmount = health / maxHealth;
     }
 }
