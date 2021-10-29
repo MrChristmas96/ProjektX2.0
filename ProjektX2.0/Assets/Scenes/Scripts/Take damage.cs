bc853220801631928488494b7fe6d01cb124d40f
SirStehn <kaste20@student.sdu.dk>
2021-10-29 10:08:37 +0200

    Added health bar with script to player
    Der er 2 errors jeg ikke kan løse

---
+++ ProjektX2.0/Assets/Scenes/Scripts/Take damage.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Takedamage : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        HealthBarScript.health -= 10f;
+    }
+}
