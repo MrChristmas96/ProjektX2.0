74dbd7b70c286d711f1c64cbfec86ddc6744b337
SirStehn <kaste20@student.sdu.dk>
2021-11-12 11:07:44 +0100

    Prøve fix

--- ProjektX2.0/Assets/Scripts/Takedamage.cs
+++ ProjektX2.0/Assets/Scripts/Takedamage.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Takedamage : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D col)
     {
+        if (col.CompareTag("Player1"))
+        {
+            Debug.Log("Ontrigger");
+        }
         HealthBarScript.health -= 10f;
     }
 }
