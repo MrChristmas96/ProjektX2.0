2c6ec02e994dc404ef33ec121cc45c8c50e394fd
Lucas1302 <lucaa21@student.sdu.dk>
2021-12-09 15:09:30 +0100

    2 shop items nu
    LULW

--- ProjektX2.0/Assets/Player1.cs
+++ ProjektX2.0/Assets/Player1.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player1 : MonoBehaviour
+public class Player1 : MonoBehaviour, IShopCustomer
 {
+    public static Player1 Instance { get; private set; }
+
+    public event EventHandler OnGoldAmountChanged;
+    public event EventHandler OnHealthPotionAmountChanged;
     // Start is called before the first frame update
     void Start()
     {
