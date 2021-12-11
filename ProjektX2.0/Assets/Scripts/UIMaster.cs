7ced1c02ef1cf2ad171cc836ff90585f80d25770
Macoh20 <macoh20@student.sdu.dk>
2021-12-11 16:59:58 +0100

    WaveStart Animation

--- ProjektX2.0/Assets/Scripts/UIMaster.cs
+++ ProjektX2.0/Assets/Scripts/UIMaster.cs
@@ -8,22 +8,31 @@ public class UIMaster : MonoBehaviour
 
     public Text p1PointsUI;
     public Text p2PointsUI;
+    public Text WaveCount;
+
+    public Transform wave;
+
+    private Animator anim;
 
     public Image P1Flash;
     public Image P2Flash;
 
     public GameMaster gameMaster;
+    public WaveSpawner waveSpawner;
 
     private void Start()
     {
         P1Flash.enabled = false;
         P2Flash.enabled = false;
+
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        p1PointsUI.text = "Player 1 points: "+ gameMaster.p1Points;
+        p1PointsUI.text = "Player 1 points: " + gameMaster.p1Points;
         p2PointsUI.text = "Player 2 points: " + gameMaster.p2Points;
+        WaveCount.text = "Wave: " + waveSpawner.waveCount;
     }
 
     public void ReactorDamageFlashP1()
@@ -36,4 +45,8 @@ public class UIMaster : MonoBehaviour
         //Play Animation
     }
 
+    public void WaveStart()
+    {
+        anim.Play("WaveStart");
+    }
 }
