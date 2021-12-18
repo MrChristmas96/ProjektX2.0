c1b6239954b8d5f7885ab5ee63961963806f2fd9
Macoh20 <macoh20@student.sdu.dk>
2021-12-18 17:01:48 +0100

    Stun player

--- ProjektX2.0/Assets/Scripts/Enemy Scripts/SendEnemy.cs
+++ ProjektX2.0/Assets/Scripts/Enemy Scripts/SendEnemy.cs
@@ -9,19 +9,33 @@ public class SendEnemy : MonoBehaviour
     public WaveSpawner waveSpawner;
     public GameMaster gameMaster;
     public string player;
+    private Rigidbody2D playerRb;
+    private Player1Controller p1Controller;
+    private Player2Controller p2Controller;
+
+    private float p1MaxMovementSpeed;
+    private float p1MaxJumpSpeed;
+    private float p2MaxMovementSpeed;
+    private float p2MaxJumpSpeed;
+
+    private int stunPrice = 10;
 
     private void Start()
     {
-        waveSpawner = FindObjectOfType < WaveSpawner >();
+        waveSpawner = FindObjectOfType <WaveSpawner>();
         gameMaster = FindObjectOfType<GameMaster>();
+        p1Controller = FindObjectOfType<Player1Controller>();
+        p2Controller = FindObjectOfType<Player2Controller>();
 
     }
 
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.CompareTag("Player1"))
         {
+            playerRb = collision.GetComponent<Rigidbody2D>();
             player = "P1";
         }
         else if (collision.CompareTag("Player2"))
@@ -35,23 +49,6 @@ public class SendEnemy : MonoBehaviour
     {
         player = "P0";
     }
-
-    /*
-
-    public void Send()
-    {
-        if (Input.GetKeyDown(KeyCode.G)) {  
-            waveSpawner.enemyP1 += gameMaster.p1Points;
-            gameMaster.p1Points = 0;
-        }
-        else if (Input.GetKeyDown(KeyCode.N))
-            {
-                Debug.Log("heeey");
-                waveSpawner.enemyP2 += gameMaster.p2Points;
-                gameMaster.p2Points = 0;
-            }
-    }
-    */
 
     public void Send()
     {
@@ -76,8 +73,58 @@ public class SendEnemy : MonoBehaviour
 
     public void Stun()
     {
-        waveSpawner.enemyP1 += gameMaster.p1Points;
-        gameMaster.p1Points = 0;
+
+        if (player == null || player == "P0")
+        {
+            Debug.Log("No player Chosen");
+        }
+        else if (player == "P1")
+        {
+            if(gameMaster.p1Points >= stunPrice)
+            {
+                gameMaster.p1Points -= stunPrice; 
+                p2MaxMovementSpeed = p2Controller.movementSpeed;
+                p2MaxJumpSpeed = p2Controller.jumpSpeed;
+                StartCoroutine(StunP2());
+            }
+            else
+            {
+                Debug.Log("Not enought Points");
+            }
+
+        }
+
+        else if (player == "P2")
+        {
+            if(gameMaster.p2Points >= stunPrice)
+            {
+                gameMaster.p2Points -= stunPrice;
+                p1MaxMovementSpeed = p1Controller.movementSpeed;
+                p1MaxJumpSpeed = p1Controller.jumpSpeed;
+                StartCoroutine(StunP1());
+            }
+            else
+            {
+                Debug.Log("Not enought Points");
+            }
+        }
     }
 
+    IEnumerator StunP2()
+    {
+        p2Controller.jumpSpeed = 0;
+        p2Controller.movementSpeed = 0;
+        yield return new WaitForSeconds(2);
+        p2Controller.jumpSpeed = p2MaxJumpSpeed;
+        p2Controller.movementSpeed = p2MaxMovementSpeed;
+    }
+
+    IEnumerator StunP1()
+    {
+        p1Controller.jumpSpeed = 0;
+        p1Controller.movementSpeed = 0;
+        yield return new WaitForSeconds(2);
+        p1Controller.jumpSpeed = p1MaxJumpSpeed;
+        p1Controller.movementSpeed = p1MaxMovementSpeed;
+    }
 }
